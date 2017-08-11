d24dfb371394bdc0baae0b16dff84752d9792d8f
伍双 <beingswu@gmail.com>
2017-08-11 13:24:11 +0800

    update

--- app/models/common/const.js
+++ app/models/common/const.js
@@ -1,3 +1,3 @@
-export const url = 'https://api.dribbble.com/v1';
+export const url = 'https://api.dribbble.com/v1'
 export const dribbbleToken =
-  '4fd8a5c420779edccdad67321d3f8e6e468db0e61e350d1445abd5037afcd035';
+  '4fd8a5c420779edccdad67321d3f8e6e468db0e61e350d1445abd5037afcd035'
--- app/models/router.js
+++ app/models/router.js
@@ -1,38 +1,38 @@
-import { NavigationActions } from 'react-navigation';
-import { routerReducer } from '../router';
+import { NavigationActions } from 'react-navigation'
+import { routerReducer } from '../router'
 
 const actions = [
-    NavigationActions.BACK,
-    NavigationActions.INIT,
-    NavigationActions.NAVIGATE,
-    NavigationActions.RESET,
-    NavigationActions.SET_PARAMS,
-    NavigationActions.URI,
-];
+  NavigationActions.BACK,
+  NavigationActions.INIT,
+  NavigationActions.NAVIGATE,
+  NavigationActions.RESET,
+  NavigationActions.SET_PARAMS,
+  NavigationActions.URI,
+]
 
 export default {
-    namespace: 'router',
-    state: {
-        ...routerReducer(),
+  namespace: 'router',
+  state: {
+    ...routerReducer(),
+  },
+  reducers: {
+    apply(state, { payload: action }) {
+      return routerReducer(state, action)
     },
-    reducers: {
-        apply(state, { payload: action }) {
-            return routerReducer(state, action);
-        },
-    },
-    effects: {
-        watch: [
-            function* watch({ take, put }) {
-                const loop = true;
-                while (loop) {
-                    const payload = yield take(actions);
-                    yield put({
-                        type: 'apply',
-                        payload,
-                    });
-                }
-            },
+  },
+  effects: {
+    watch: [
+      function* watch({ take, put }) {
+        const loop = true
+        while (loop) {
+          const payload = yield take(actions)
+          yield put({
+            type: 'apply',
+            payload,
+          })
+        }
+      },
       { type: 'watcher' },
-        ],
-    },
-};
+    ],
+  },
+}
