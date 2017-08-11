8a2b5b635fa110a7fbfbd755cde4bac111caeffa
伍双 <beingswu@gmail.com>
2017-08-11 13:10:12 +0800

    update

--- app/index.js
+++ app/index.js
@@ -1,24 +1,24 @@
 import React from 'react';
-import { AppRegistry, AsyncStorage } from 'react-native';
+import {AppRegistry, AsyncStorage} from 'react-native';
 import dva from 'dva/mobile';
-import { persistStore, autoRehydrate } from 'redux-persist';
+import {persistStore, autoRehydrate} from 'redux-persist';
 
-import { registerModels } from './models';
+import {registerModels} from './models';
 import Router from './router';
 
 const app = dva({
     initialState: {},
     extraEnhancers: [autoRehydrate()],
     onError(e) {
-    /* eslint-disable */
-    console.log('onError', e)
-  },
-})
-registerModels(app)
-app.router(() => <Router />)
-const App = app.start()
+        /* eslint-disable */
+        console.log('onError', e);
+    },
+});
+registerModels(app);
+app.router(() => <Router/>);
+const App = app.start();
 
 // eslint-disable-next-line no-underscore-dangle
-persistStore(app._store, { storage: AsyncStorage })
+persistStore(app._store, {storage: AsyncStorage});
 
-AppRegistry.registerComponent('DvaStarter', () => App)
+AppRegistry.registerComponent('DvaStarter', () => App);
